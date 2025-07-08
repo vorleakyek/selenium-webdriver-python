232f3af04b8b60bc533f55f96b7dc3d47fd01ebd
Vorleak Yek <vorleakyek@gmail.com>
2025-07-08 16:14:11 -0700

    Update cookie-clicker.py

--- cookie-clicker.py
+++ cookie-clicker.py
@@ -27,16 +27,38 @@ except NoSuchElementException:
 
 cookie = driver.find_element(by=By.ID, value="bigCookie")
 
-
+end_game = False
+start_game_time = time.time()
 start_time = time.time()
 
-while time.time() - start_time <=5:
-    for i in range(100):
+while end_game != True:
+
+    while time.time() - start_time <= 5:
         cookie.click()
-# start_time = time.time()
+
+    points = driver.find_element(By.ID, "cookies").text.split()[0]
+    points_int = int(points.replace(",", ""))
+
+    item_upgrade_location = driver.find_elements(By.CSS_SELECTOR, "#products .product.unlocked.enabled")[-1]
+    item_upgrade = driver.find_elements(By.CSS_SELECTOR, "#products .unlocked .price")[-1]
+    item_price = item_upgrade.text
+    item_price_int = int(item_price.replace(",", ""))
+
+    if points_int >= item_price_int:
+        item_upgrade_location.click()
+
+    start_time = time.time()
+
+    if time.time() - start_game_time > 10:
+        # cookies_per_second = driver.find_element(By.ID, "cookiesPerSecond").text.split(":")[-1].strip()
+        # print(cookies_per_second)
+        end_game = True
 
 
-points = driver.find_element(By.ID, "cookies").text.split()[0]
-print(points)
 
 
+
+
+
+
+# driver.quit()
